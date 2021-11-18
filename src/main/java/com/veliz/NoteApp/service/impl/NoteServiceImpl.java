5dd5ab0977c0e22d20f39ab67c0d22e0374fc4aa
EriVelk <erve@live.com.mx>
2021-11-17 18:38:16 -0600

    Test

--- src/main/java/com/veliz/NoteApp/service/impl/NoteServiceImpl.java
+++ src/main/java/com/veliz/NoteApp/service/impl/NoteServiceImpl.java
@@ -3,9 +3,13 @@ package com.veliz.NoteApp.service.impl;
 import com.veliz.NoteApp.dao.NoteDAO;
 import com.veliz.NoteApp.dao.UserDAO;
 import com.veliz.NoteApp.dto.NoteDTO;
+import com.veliz.NoteApp.entity.Note;
+import com.veliz.NoteApp.entity.User;
 import com.veliz.NoteApp.mapper.NoteMapper;
 import com.veliz.NoteApp.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -32,7 +36,12 @@ public class NoteServiceImpl implements NoteService {
 
     @Override
     public List<NoteDTO> listNote(long id) {
-        return noteMapper.noteListToNoteDTOList((noteDAO.listNote(id)));
+        Note note = new Note();
+        User user = new User();
+        user.setId(id);
+        note.setUser(user);
+        Example<Note> example = Example.of(note);
+        return noteMapper.noteListToNoteDTOList(noteDAO.findAll(example));
     }
 
     @Override
